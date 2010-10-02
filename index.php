c8853282a225f52430c576fa3a49f92903722c4e
Colm Doyle <colm@colmisainmdom.com>
2010-10-02 21:36:41 +0100

    Initial Upload
    Signed-off-by: Colm Doyle <colm@colmisainmdom.com>

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<title>Colm is ainm dom</title>
+		
+		<!-- Meta Tags -->
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		
+		<!-- include javascript -->
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+		<script type="text/javascript" src="javascript/colmisainmdom.jquery.js"></script>
+		
+		<!-- include CSS (Blueprint) -->
+		<link rel="stylesheet" href="css/blueprint/screen.css" type="text/css" media="screen, projection">
+		<link rel="stylesheet" href="css/blueprint/print.css" type="text/css" media="print">
+		<!--[if lt IE 8]>
+		  <link rel="stylesheet" href="css/blueprint/ie.css" type="text/css" media="screen, projection">
+		<![endif]-->
+	</head>
+	
+	<body>
+	
+	<!-- include FB Javascript SDK -->
+	<div id="fb-root"></div>
+	<script type="text/javascript">
+	  window.fbAsyncInit = function() {
+	    FB.init({appId: '120999667956026', status: true, cookie: true,
+	             xfbml: true});
+	  };
+	  (function() {
+	    var e = document.createElement('script'); e.async = true;
+	    e.src = document.location.protocol +
+	      '//connect.facebook.net/en_US/all.js';
+	    document.getElementById('fb-root').appendChild(e);
+	  }());
+	</script>
+
+	<div class="container">
+	<h1>Colm Doyle</h1>
+	<p><a href="#" rel="colm/colmisainmdom.com" class="email">email me</a></p>	
+	<p><fb:like href="http://www.colmisainmdom.com/index.php" font="arial"></fb:like></p>
+	
+	</div>
+	</body>
+</html>
+
