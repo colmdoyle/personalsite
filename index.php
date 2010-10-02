e34fc7a56b77eb5dffcdb116dfaf3066c9e95c72
Colm Doyle <colm@colmisainmdom.com>
2010-10-02 21:50:15 +0100

    Added Google Analytics
    Signed-off-by: Colm Doyle <colm@colmisainmdom.com>

--- index.php
+++ index.php
@@ -16,6 +16,21 @@
 		<!--[if lt IE 8]>
 		  <link rel="stylesheet" href="css/blueprint/ie.css" type="text/css" media="screen, projection">
 		<![endif]-->
+		
+		<!-- Urchin -->
+		<script type="text/javascript">
+		
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-2312829-1']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		
+		</script>
 	</head>
 	
 	<body>
@@ -36,10 +51,11 @@
 	</script>
 
 	<div class="container">
-	<h1>Colm Doyle</h1>
-	<p><a href="#" rel="colm/colmisainmdom.com" class="email">email me</a></p>	
-	<p><fb:like href="http://www.colmisainmdom.com/index.php" font="arial"></fb:like></p>
-	
+		<div class="span-24 showgrid">
+			<h1>Colm Doyle</h1>
+			<p><a href="#" rel="colm/colmisainmdom.com" class="email">email me</a></p>	
+			<p><fb:like href="http://www.colmisainmdom.com/index.php" font="arial"></fb:like></p>
+		</div>
 	</div>
 	</body>
 </html>
