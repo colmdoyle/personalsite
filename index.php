03d1ce904a6b5b4e5bfa9bd0fa831eaff1fa324f
Colm Doyle <colm@colmisainmdom.com>
2010-10-02 23:54:36 +0100

    Removed lipsum
    Signed-off-by: Colm Doyle <colm@colmisainmdom.com>

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
 	<div id="content" class="container">
 		<div class="span-24 last">
 			<hr>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Nothing to see here, just move along.</p>
 		</div>
 	</div>
 	<div id="footer" class="container">
