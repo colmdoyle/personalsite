f79bb900464458896a574977ce1837ca75516bba
Colm Doyle <colm@colmisainmdom.com>
2010-10-03 00:00:59 +0100

    Added a custom css file & ref'd same in index.php
    Signed-off-by: Colm Doyle <colm@colmisainmdom.com>

--- index.php
+++ index.php
@@ -22,13 +22,14 @@
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 		<script type="text/javascript" src="javascript/colmisainmdom.jquery.js"></script>
 		
-		<!-- include CSS (Blueprint) -->
+		<!-- include CSS (Mostly Blueprint) -->
 		<link rel="stylesheet" href="css/blueprint/screen.css" type="text/css" media="screen, projection">
 		<link rel="stylesheet" href="css/blueprint/print.css" type="text/css" media="print">
 		<!--[if lt IE 8]>
 		  <link rel="stylesheet" href="css/blueprint/ie.css" type="text/css" media="screen, projection">
 		<![endif]-->
-		
+		<link rel="stylesheet" href="css/custom.css" type="text/css" media="screen, projection">
+
 		<!-- Urchin -->
 		<script type="text/javascript">
 		
