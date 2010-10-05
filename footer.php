72092b8410e1ce10b16d556af17e1d40b3eeac72
Colm Doyle <colm@colmisainmdom.com>
2010-10-05 23:15:29 +0100

    Conversion from plain HTML to PHP powered.
    Split out template into a number of php files that you can require() to make certain sections of the website consistent when changed.
    
    Signed-off-by: Colm Doyle <colm@colmisainmdom.com>

---
+++ footer.php
@@ -0,0 +1,29 @@
+<!-- BEGIN footer.php -->
+
+	<div id="footer" class="container">
+		<hr/>
+		<div class="span-7 colborder">
+			<h5>My life online</h5>
+			<ul>
+				<li><a href="http://ie.linkedin.com/in/colmdoyle">LinkedIn</a></li>
+				<li><a href="http://github.com/colmdoyle">GitHub</a></li>
+				<li><a href="http://twitter.com/colmisainmdom">Twitter</a></li>
+				<li><a href="http://www.facebook.com/colmdoyle">Facebook</a></li>
+			</ul>
+		</div>
+		<div class="span-7 colborder">
+			<h5>Projects</h5>
+			<ul>
+				<li>Facebook Applications</li>
+				<li>Link 2</li>
+				<li>Link 3</li>
+				<li>Link 4</li>
+			</ul>
+		</div>
+		<div class="span-8 last">
+			<a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/"><img alt="Creative Commons Licence" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" /></a><br />This work by <a xmlns:cc="http://creativecommons.org/ns#" href="http://www.colmisainmdom.com/" property="cc:attributionName" rel="cc:attributionURL">Colm Doyle</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.
+		</div>
+	</div>
+	</body>
+</html>
+<!-- END footer.php -->
