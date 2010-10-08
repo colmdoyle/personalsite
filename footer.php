f81956cc6537dc68a0d15292b2fd37d9879d2281
Colm Doyle <colm@colmisainmdom.com>
2010-10-08 22:16:06 +0100

    Added link to sample FB app source on GitHub
    Signed-off-by: Colm Doyle <colm@colmisainmdom.com>

--- footer.php
+++ footer.php
@@ -12,10 +12,10 @@
 			</ul>
 		</div>
 		<div class="span-7 colborder">
-			<h5>Projects</h5>
+			<h5>Coding Projects</h5>
 			<ul>
-				<li><a href="http://github.com/colmdoyle/Colm-is-ainm-dom">This site!</a></li>
-				<li>Link 2</li>
+				<li><a href="http://github.com/colmdoyle/Colm-is-ainm-dom">This site! (source)</a></li>
+				<li><a href="http://github.com/colmdoyle/Facebook-Sample-Canvas-Application">A really simple Facebook sample application (source)</a></li>
 				<li>Link 3</li>
 				<li>Link 4</li>
 			</ul>
