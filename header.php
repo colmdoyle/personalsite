498e1695f033d9a6de5852c5661e980694ba148a
Colm Doyle <colm@colmisainmdom.com>
2012-10-21 05:53:05 +0000

    auto fb picture

--- header.php
+++ header.php
@@ -13,10 +13,10 @@ require('functionFile.php');
 	<head>
 		<title>Colm is ainm dom - The personal site of Colm Doyle</title>
 	    <link rel="shortcut icon" href="http://www.colmisainmdom.com/favicon.ico" />
-				
+
 		<!-- Meta Tags -->
 		<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-		
+
 		<!-- OG Tags -->
 	    <meta property="og:title" content="Colm is ainm dom"/>
 	    <meta property="og:type" content="website"/>
@@ -27,11 +27,11 @@ require('functionFile.php');
     	<meta property="og:description" content="The website of a twenty something developer from Dublin, Ireland"/>
 
 
-    	
+
 		<!-- include javascript -->
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 		<script type="text/javascript" src="javascript/colmisainmdom.jquery.js"></script>
-		
+
 		<!-- include CSS (Mostly Blueprint) -->
 		<link rel="stylesheet" href="css/blueprint/screen.css" type="text/css" media="screen, projection"/>
 		<link rel="stylesheet" href="css/blueprint/print.css" type="text/css" media="print"/>
@@ -43,22 +43,22 @@ require('functionFile.php');
 
 		<!-- Urchin -->
 		<script type="text/javascript">
-		
+
 		  var _gaq = _gaq || [];
 		  _gaq.push(['_setAccount', 'UA-2312829-1']);
 		  _gaq.push(['_trackPageview']);
-		
+
 		  (function() {
 		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 		  })();
-		
+
 		</script>
 	</head>
-	
+
 	<body>
-	
+
 	<!-- include FB Javascript SDK -->
 	<div id="fb-root"></div>
 	<script type="text/javascript">
@@ -80,8 +80,8 @@ require('functionFile.php');
 			<h4>A twenty something developer from Dublin, Ireland</h4>
 		</div>
 		<div id="right_header" class="span-3 last">
-			<img src="img/headshot.jpg" alt="Photo of Colm Doyle" width="75" height="75" />
+			<img src="http://graph.facebook.com/colmdoyle/picture?width=75&height=75" alt="Photo of Colm Doyle" width="75" height="75" />
 		</div>
 		<hr/>
-	</div>	
+	</div>
 <!-- END header.php -->
