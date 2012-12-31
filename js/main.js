3ac01f835694a54d2ae06daff1c913260e22651d
Colm Doyle <colm@colmisainmdom.com>
2012-12-31 06:48:25 +0000

    add a fade to the footer

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+	$('#footer-note').hover(
+		function() {
+			$('#footer-note').addClass('in');
+		},
+		function() {
+			$('#footer-note').removeClass('in');
+		}
+	);
+
+});
