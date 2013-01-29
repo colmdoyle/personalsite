c29b32744b9ebdb3959ce1d52c0a2e145499e7fb
Colm Doyle <colm@colmisainmdom.com>
2013-01-29 02:35:34 +0000

    animate services on hover

--- js/main.js
+++ js/main.js
@@ -8,5 +8,29 @@ $(document).ready(function () {
 			$('#footer-note').removeClass('in');
 		}
 	);
+	
+	$('#social-links li a').hover(
+		function(e) {
+			var service = $(this).attr('title');
+			$('#title').animate({'opacity': 0}, 100, function () {
+				$(this).text(service);
+			}).animate({'opacity': 1}, 100);
+		},
+		function(e) {
+		}
+	);
+	
+	$('#social-links').hover(
+		function() {
+			
+		},
+		function() {
+			if ($('#title').text() != 'Developer Support Engineer @ Facebook')  {
+			$('#title').animate({'opacity': 0}, 100, function () {
+				$(this).text('Developer Support Engineer @ Facebook');
+			}).animate({'opacity': 1}, 100);
+			}
+		}
+	)
 
 });
